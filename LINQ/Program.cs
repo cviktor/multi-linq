82c530a44e177edf60db887a0689d2d735dc1968
Czeglédi Viktor <czegledi.viktor@gmail.com>
2018-11-19 20:00:34 +0100

    Delegate példa

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace LINQ
 {
+    delegate bool FilterDelegate(int number);
+
     class Program
     {
         static void Main(string[] args)
@@ -49,14 +51,59 @@ namespace LINQ
             //numbers.Add()-ot
             var numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            var evenNumbers = FilterEven(numbers);
+            //var evenNumbers = FilterEven(numbers);
 
-            foreach (var num in evenNumbers)
+            //foreach (var num in evenNumbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //var oddNumbers = FilterOdd(numbers);
+
+            //foreach (var num in oddNumbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //fontos hogy az IsEven mögé nem írjuk oda a ()-eket
+            //mert akkor meghívjuk és az eredményét adjuk be paraméterbe
+
+            FilterDelegate f = MyMethod;
+
+            var filtered = Filter(numbers, f);
+
+            foreach (var num in filtered)
             {
                 Console.WriteLine(num);
             }
 
             Console.ReadKey();
+        }
+
+        static bool MyMethod(int x)
+        {
+            return x > 3 && x % 2 == 0 && x < 10;
+        }
+
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        static List<int> Filter(List<int> nums, FilterDelegate del)
+        {
+            var result = new List<int>();
+
+            foreach (var num in nums)
+            {
+                bool res = del(num);
+                if (res)
+                {
+                    result.Add(num);
+                }
+            }
+
+            return result;
         }
 
         static List<int> FilterEven(List<int> nums)
@@ -66,6 +113,36 @@ namespace LINQ
             foreach (var num in nums)
             {
                 if (num % 2 == 0)
+                {
+                    result.Add(num);
+                }
+            }
+
+            return result;
+        }
+
+        static List<int> FilterOdd(List<int> nums)
+        {
+            var result = new List<int>();
+
+            foreach (var num in nums)
+            {
+                if (num % 2 == 1)
+                {
+                    result.Add(num);
+                }
+            }
+
+            return result;
+        }
+
+        static List<int> FilterGreaterThan(List<int> nums, int min)
+        {
+            var result = new List<int>();
+
+            foreach (var num in nums)
+            {
+                if (num > min)
                 {
                     result.Add(num);
                 }
